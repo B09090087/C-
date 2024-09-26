62ba9bbfbd6b9de3c5b32a834b4ffcaa1e0c2846
B09090087 <82439820+B09090087@users.noreply.github.com>
2024-09-27 00:26:46 +0800

    Add files via upload

---
+++ string字串用法/string字串用法/Program.cs
@@ -0,0 +1,28 @@
+﻿//字串常見用法
+
+//換行
+System.Console.WriteLine("Hello \nMr.Shih");
+//加符號
+System.Console.WriteLine("Hello\" Mr.Shih");
+
+string phrase = "Hello Mr.Shih";
+//字串長度
+System.Console.WriteLine(phrase.Length);
+System.Console.WriteLine("Hello Mr.Shih".Length);
+//大寫
+System.Console.WriteLine(phrase.ToUpper());
+//小寫
+System.Console.WriteLine(phrase.ToLower());
+//是否含有特定的子字串
+System.Console.WriteLine(phrase.Contains("Hello"));
+System.Console.WriteLine(phrase.Contains("QQ"));
+//字串特定位置的值
+System.Console.WriteLine(phrase[0]); //從0開始算
+System.Console.WriteLine(phrase[6]);
+//用值去找位置
+System.Console.WriteLine(phrase.IndexOf('H'));
+System.Console.WriteLine(phrase.IndexOf("Shih"));
+System.Console.WriteLine(phrase.IndexOf("QQ"));
+//字串切割
+System.Console.WriteLine(phrase.Substring(2));
+System.Console.WriteLine(phrase.Substring(5,5));
