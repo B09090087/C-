7b6ccc5fd400627bab72c002a44791cfc956f1c3
B09090087 <82439820+B09090087@users.noreply.github.com>
2024-09-27 00:36:38 +0800

    Add files via upload

---
+++ class 、 object 類別 物件/class 、 object 類別 物件/Program.cs
@@ -0,0 +1,15 @@
+﻿// class、object 類別 物件
+
+
+Person person1 = new Person();
+person1.height = 169.8;
+person1.age = 22;
+person1.name = "石頭";
+
+Person person2 = new Person();
+person2.height = 160.8;
+person2.age = 22;
+person2.name = "小白";
+
+System.Console.WriteLine(person1.age);
+System.Console.WriteLine(person1.name);
---
+++ for迴圈/for迴圈/Program.cs
@@ -0,0 +1,9 @@
+﻿// for迴圈
+
+int[] nums = { 12, 456, 78, 56, 89 , 1 , 23 , 56 ,47 , 77};
+
+
+for(int i = 0; i < nums.Length; i++)
+{
+    System.Console.WriteLine(nums[i]);
+}
