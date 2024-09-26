380604064d6543270244a4d48affcb3e73bd8795
B09090087 <82439820+B09090087@users.noreply.github.com>
2024-09-27 00:27:53 +0800

    Add files via upload

---
+++ 取得用戶輸入/取得用戶輸入/Program.cs
@@ -0,0 +1,7 @@
+﻿// 取得用戶輸入
+
+System.Console.Write("請輸入你的名字 : ");
+string name = System.Console.ReadLine();
+System.Console.Write("請輸入你的年紀 : ");
+string age = System.Console.ReadLine();
+System.Console.WriteLine("你好啊，" + name + "。" + "你今年" + age + "歲");
---
+++ 數字用法/數字用法/Program.cs
@@ -0,0 +1,21 @@
+﻿// 數字常見用法(整數 浮點數)
+
+System.Console.WriteLine(5 + 2);
+System.Console.WriteLine(5 - 2);
+System.Console.WriteLine(5 * 2);
+System.Console.WriteLine(5 / 2);
+System.Console.WriteLine(100 ^ 3);
+System.Console.WriteLine(100 & 3);
+
+//絕對值
+System.Console.WriteLine(System.Math.Abs(-10));
+//次方
+System.Console.WriteLine(System.Math.Pow(2,3));
+//開根號
+System.Console.WriteLine(System.Math.Sqrt(144));
+//最大最小
+System.Console.WriteLine(System.Math.Max(2,100));
+System.Console.WriteLine(System.Math.Min(2,100));
+//四捨五入
+System.Console.WriteLine(System.Math.Round(3.4));
+System.Console.WriteLine(System.Math.Round(3.6));
